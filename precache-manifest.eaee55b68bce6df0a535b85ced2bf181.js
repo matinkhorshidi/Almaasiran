7d498ede3a783079e2289d63674f1dc816a21844
unknown <khorshidimatin@gmail.com>
2020-07-06 22:02:04 +0430

    Updates

---
+++ precache-manifest.eaee55b68bce6df0a535b85ced2bf181.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fba80d2123f420592458911c8efa56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "765bd5d78b1ce16556eb",
+    "url": "/static/css/main.52e809dc.chunk.css"
+  },
+  {
+    "revision": "02006c99af3b1849efbf",
+    "url": "/static/js/2.0271e7b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0271e7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765bd5d78b1ce16556eb",
+    "url": "/static/js/main.f565bab6.chunk.js"
+  },
+  {
+    "revision": "71684afbeb30a1ea1b7e",
+    "url": "/static/js/runtime-main.ef567449.js"
+  },
+  {
+    "revision": "45900fbaf914e966fa5c5f0f162dbb76",
+    "url": "/static/media/almas.45900fba.png"
+  },
+  {
+    "revision": "bdc5043ef58baa84ebf18f01a6fed3fc",
+    "url": "/static/media/instagram-sketched.bdc5043e.svg"
+  },
+  {
+    "revision": "240456b78ce6d099216a44926824231f",
+    "url": "/static/media/phone.240456b7.svg"
+  },
+  {
+    "revision": "a16c7a657d3a3594fc17f14791047ed8",
+    "url": "/static/media/pic.a16c7a65.jpg"
+  },
+  {
+    "revision": "adc5c2acebc903f5636f4aa133bfdf53",
+    "url": "/static/media/pin.adc5c2ac.svg"
+  },
+  {
+    "revision": "64f34638dc9da757feeec5bedb3151a2",
+    "url": "/static/media/settingsicon.64f34638.svg"
+  }
+]);
