7bab7fcd431f282c03d52e478d2a5e9fc6ee6fcf
unknown <khorshidimatin@gmail.com>
2020-07-08 00:03:38 +0430

    My Last commit 2

--- src/App.js
+++ src/App.js
@@ -42,15 +42,15 @@ function App() {
           >almaas_iran</a>
 
           <a
-            href="https://www.instagram.com/almaas_iran"
-            style={{ marginLeft: '22vw', marginTop: '8.5vmin' }}
+            href=""
+            style={{ marginLeft: '26vw', marginTop: '8.5vmin' }}
             className="Instagram-link"
           >بابلسر نرسیده به دریاکنار</a>
           <img src={pin} alt="logo" style={{ marginLeft: '0.2vw', marginTop: '9vmin', height: ' 2.2vw' }} />
 
           <a
-           href="https://www.instagram.com/almaas_iran"
-            style={{ marginTop: '8.5vmin', marginLeft: '22vw' }}
+           href=""
+            style={{ marginTop: '8.5vmin', marginLeft: '24vw' }}
             className="Instagram-link"
           >09017711751</a>
           <img src={phone} alt="logo" style={{ marginTop: '9vmin', height: ' 2.2vw' }} />
